dddf0bde84c033cd941b319c1047085a9b216129
Tevin-creator <isaactevin67@gmail..com>
2021-10-07 19:43:57 +0300

    added layout design files in res folder

---
+++ app/src/main/java/com/tev/book_a_meal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tev.book_a_meal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
